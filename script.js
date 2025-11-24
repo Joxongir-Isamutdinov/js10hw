1cbbad38c06481a5707bf17197602038632eee61
Joxa <joraevjoxongir2@gmail.com>
2025-11-24 20:29:40 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let array = ["apple", "banana", "orange", "pinapple"];
+
+if (confirm("Siz arrayingizdan bitta elementni olib tashlansinmi?")) {
+    let ochir = array.pop();
+    alert(`${ochir}arrayda ${array.length} ta element qoldi.`);
+} else {
+    alert(`Arrayda ${array.length} ta element bor.`);
+}
+console.log(array);
